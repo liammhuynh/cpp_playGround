927c5491578c7dd419354478f3396b20c69ea75c
thuymhuynh <77704009+thuymhuynh@users.noreply.github.com>
2021-09-19 14:55:02 -0500

    Create 6.12.2: Resizing a vector.cpp

---
+++ 6. Arrays and Vectors/6.12.2: Resizing a vector.cpp
@@ -0,0 +1,31 @@
+/* Resize vector countDown to have newSize elements. Populate the vector with integers {newSize, newSize - 1, ..., 1}. 
+Ex: If newSize = 3, then countDown = {3, 2, 1}, and the sample program outputs:
+3 2 1 Go! */
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+   vector<int> countDown(0);
+   int newSize;
+   unsigned int i;
+
+   cin >> newSize;
+
+   /* Your solution goes here  */
+countDown.resize(newSize);
+
+	for(i = 0; i < newSize; i++){
+		countDown.at(i) = newSize;
+		countDown.at(i) = newSize - i;
+	}
+
+   for (i = 0; i < countDown.size(); ++i) {
+      cout << countDown.at(i) << " ";
+   }
+   cout << "Go!" << endl;
+
+   return 0;
+}
