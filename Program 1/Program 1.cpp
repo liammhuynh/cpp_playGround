044216a5646ee083440079c97775a1fc1b373ea4
thuymhuynh <77704009+thuymhuynh@users.noreply.github.com>
2021-09-22 15:16:31 -0500

    Update Program 1.cpp

--- Program 1/Program 1.cpp
+++ Program 1/Program 1.cpp
@@ -1,22 +1,3 @@
-// write C++ program to roll a six-sided dice once and then we roll a second dice as many times as is necessary to obtain the same face as the one obtained in the first dice.
-
-/* Rules of the Game:
-1- Randomly roll the first dice once (six-sided dice).
-2- Randomly roll the second dice.
-3- Compare first and second dice:
-a. If they are equal in less than 3 rounds, announce that you got a match and that
-computer won.
-b. If they are not equal in less than 3 rounds, keep checking for a match and then
-announce that you got a match in “X” rounds and that the computer lost this turn.
-c. Note: First dice only roll randomly once and with each round the second dice will
-roll randomly until you get a match.
-4- After you announce either the computer won or lost, you will ask the user if they want to
-have another turn.
-a. If the user wanted to have another turn, then you will repeat all previous steps.
-b. If user decided to end the game, then output a message to the user “Have a
-great day” and terminate the program. */
-
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib> 
@@ -46,7 +27,7 @@ int main()
 	dice_1 = Dice(roll_1);
 	dice_2 = Dice(roll_2);
 
-	cout << endl;   //looks easier on eyes when user play another turn
+	cout << endl;  
 
 		while (rounds <= 3)
 		{
